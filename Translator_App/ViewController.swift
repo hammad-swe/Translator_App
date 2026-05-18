72a4d121d673f2f45d2028640551f23627f05bad
Hammad Ali <hammadali78056@gmail.com>
2026-05-18 10:02:30 +0500

    Initial Commit

---
+++ Translator_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translator_App
+//
+//  Created by Hammad Ali on 18/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
